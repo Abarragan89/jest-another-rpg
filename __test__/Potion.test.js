39abed0532eb48380929eea9c7a748f379e3b5c6
Anthony Barragan <anthony.bar.89@gmail.com>
2022-02-20 14:04:47 -0800

    finished player object. Added inventory and use a potion methods

--- __test__/Potion.test.js
+++ __test__/Potion.test.js
@@ -3,7 +3,6 @@ const Potion = require('../lib/Potion')
 test('creates a health potion object', () => {
     // make an instance of an object and test the values with given parameters
     const potion = new Potion();
-
     expect(potion.name).toEqual(expect.any(String));
     expect(potion.name.length).toBeGreaterThan(0);
     expect(potion.value).toEqual(expect.any(Number));
